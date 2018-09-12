1513577ce6b18d8d2217803e024f494635595d30
sem <pumidon@proteus-tech.com>
2018-09-12 19:49:24 +0700

    Edit setup config and restructure

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -12,7 +12,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Semooze/csj_converter",
-    packages=find_packages(),
+    packages=['csj_converter'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
